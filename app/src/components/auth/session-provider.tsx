60407e0942ed29e3a769beb32726afc75068e408
Jorge Miguel <46396207+jhfmiguel@users.noreply.github.com>
2026-09-22 15:34:28 -0300

    082 - Migra controles operacionais para UI própria

--- app/src/components/auth/session-provider.tsx
+++ app/src/components/auth/session-provider.tsx
@@ -2,7 +2,7 @@
 
 import * as React from "react";
 import { usePathname, useRouter } from "next/navigation";
-import { Button } from "@primereact/ui/button";
+import { Button } from "components/ui/button";
 import { Message } from "components/common/message";
 import { httpClient } from "api/http";
 
