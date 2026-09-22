9c4389bbe2209ce8f028224fea69f10899a99302
Jorge Miguel <46396207+jhfmiguel@users.noreply.github.com>
2026-09-22 15:33:20 -0300

    082 - Migra botões operacionais para UI própria

--- app/src/components/erp/custody/index.tsx
+++ app/src/components/erp/custody/index.tsx
@@ -3,7 +3,7 @@
 import * as React from "react";
 import axios from "axios";
 import { useRouter } from "next/navigation";
-import { Button } from "@primereact/ui/button";
+import { Button } from "components/ui/button";
 import { Layout } from "components/layout";
 import { Message } from "components/common/message";
 import { useSession } from "components/auth/session-provider";
