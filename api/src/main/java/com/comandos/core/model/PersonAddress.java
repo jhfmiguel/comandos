901600c4ac2c322dae8ec5e1a29c7c9900ebb702
Jorge Miguel <46396207+jhfmiguel@users.noreply.github.com>
2026-09-23 19:25:13 -0300

    288 - Adapta endereços de pessoa ao Oracle

--- api/src/main/java/com/comandos/core/model/PersonAddress.java
+++ api/src/main/java/com/comandos/core/model/PersonAddress.java
@@ -8,10 +8,10 @@ public class PersonAddress extends CoreEntity {
     @ManyToOne(optional = false)
     @JoinColumn(name = "person_id", nullable = false, updatable = false)
     public Person person;
-    @Column(nullable = false) public String type;
+    @Column(name = "address_type", nullable = false) public String type;
     @Column(nullable = false) public String postalCode;
     @Column(nullable = false) public String street;
-    @Column(nullable = false) public String number;
+    @Column(name = "address_number", nullable = false) public String number;
     public String complement;
     public String district;
     @Column(nullable = false) public String city;
