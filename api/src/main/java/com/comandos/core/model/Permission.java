a06b64f6ecc488d2d883fd23434abc7913b223d4
Jorge Miguel <46396207+jhfmiguel@users.noreply.github.com>
2026-09-23 19:18:20 -0300

    287 - Evita palavra reservada Oracle em permissões

--- api/src/main/java/com/comandos/core/model/Permission.java
+++ api/src/main/java/com/comandos/core/model/Permission.java
@@ -4,10 +4,10 @@ import jakarta.persistence.*;
 import java.time.LocalDate;
 
 @Entity
-@Table(name = "erp_permission", uniqueConstraints = {@UniqueConstraint(columnNames = {"resource", "action"})})
+@Table(name = "erp_permission", uniqueConstraints = {@UniqueConstraint(columnNames = {"resource_name", "action"})})
 public class Permission extends CoreEntity {
     
-	@Column(name = "resource", nullable = false, length = 255)
+	@Column(name = "resource_name", nullable = false, length = 255)
     public String resource;
     
 	@Column(name = "action", nullable = false, length = 255)
