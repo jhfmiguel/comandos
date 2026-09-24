004488940dca56c2893db5f7ad81b0daf6331e2a
Jorge Miguel <46396207+jhfmiguel@users.noreply.github.com>
2026-09-24 13:10:18 -0300

    566 - Add permission resource parameter

---
+++ api/src/main/java/com/comandos/core/model/PermissionResource.java
@@ -0,0 +1,23 @@
+package com.comandos.core.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(
+    name = "erp_permission_resource",
+    uniqueConstraints = {@UniqueConstraint(columnNames = {"code"})}
+)
+public class PermissionResource extends CoreEntity {
+
+    @Column(name = "code", nullable = false, length = 255)
+    public String code;
+
+    @Column(name = "name", nullable = false, length = 255)
+    public String name;
+
+    @Column(name = "description", length = 255)
+    public String description;
+
+    @Column(name = "active", nullable = false)
+    public Boolean active = true;
+}
