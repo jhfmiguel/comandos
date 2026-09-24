96d165dc7dc636aa509dfa10d57a455b6f915409
Jorge Miguel <46396207+jhfmiguel@users.noreply.github.com>
2026-09-24 10:20:43 -0300

    516 - Use composed selects in sales

--- app/src/components/erp/sales/index.tsx
+++ app/src/components/erp/sales/index.tsx
@@ -3,6 +3,7 @@
 import * as React from "react";
 import axios from "axios";
 import { Button } from "components/common/button";
+import { ComandosSelectField } from "components/common/select-field";
 import { Layout } from "components/layout";
 import { Message } from "components/common/message";
 import { useSession } from "components/auth/session-provider";
