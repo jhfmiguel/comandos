44bc8c17c510bb080df27f024bf031feccd06277
Jorge Miguel <46396207+jhfmiguel@users.noreply.github.com>
2026-09-23 22:18:57 -0300

    346 - Estrutura contatos múltiplos de pessoas

--- api/src/main/java/com/comandos/core/model/PersonAddress.java
+++ api/src/main/java/com/comandos/core/model/PersonAddress.java
@@ -9,13 +9,15 @@ public class PersonAddress extends CoreEntity {
     @JoinColumn(name = "person_id", nullable = false, updatable = false)
     public Person person;
     @Column(name = "address_type", nullable = false) public String type;
-    @Column(nullable = false) public String postalCode;
+    @Column public String postalCode;
     @Column(nullable = false) public String street;
     @Column(name = "address_number", nullable = false) public String number;
     public String complement;
     public String district;
     @Column(nullable = false) public String city;
     @Column(nullable = false) public String state;
+    @Column(name = "country", length = 120) public String country = "Brasil";
+    @Column(name = "foreign_address") public Boolean foreignAddress = false;
     @Column(nullable = false) public Boolean primaryAddress;
     @Column(nullable = false) public boolean archived = false;
 }
